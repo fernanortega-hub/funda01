1e5f6dd09a95d08f39502821cb735530a266ca77
fernanortega16 <fernanantonio.sv9394@gmail.com>
2020-05-11 18:07:37 -0600

    ejercicio 2

---
+++ tareaejr2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main (){
+
+    int numero1 ,  numero2;
+    
+    cout<< "Ingrese numero 1: " <<endl;
+    cin>>numero1;
+
+    cout<< "Ingrese numero 2: " <<endl;
+    cin>>numero2;
+
+    if (numero1 % numero2 == 0) {
+        cout<<"El NUMERO ES DIVISIBLE";
+
+    } else {
+        cout<<"NO ES DIVISIBLE";
+    }
+
+    return 0;
+}
+
+
