40d677b5f1cf1bd89b86540785edd43ff6a54b05
fernanortega16 <fernanantonio.sv9394@gmail.com>
2020-06-27 17:52:16 -0600

    Promedio Estatura

--- corto4/ejercicio2.cpp
+++ corto4/ejercicio2.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-float estaturaPromedio(int n, float suma, float altura[]){
+float estaturaPromedio(int n, float suma, float altura[]){//Función para la estatura promedio de los estudiantes.
     float promedio;
     int arriba=0, abajo=0;
-    cout << "Cu"<<char(160)<<"ntas alturas desea promediar: " <<endl;
+    cout << "Cu"<<char(160)<<"ntas alturas desea promediar: " <<endl;//Opcion para que el usuario ocupe menos variables de las que necesita
     cin >> n;
 
-    if(n>25){
+    if(n>25){//Condicional para que el usuario no sobre pase el limite de estudiantes
         cout << "No puede sobrepasar el limite del arreglo."<<endl;
     }
     else{
@@ -26,14 +26,14 @@ float estaturaPromedio(int n, float suma, float altura[]){
 
     cout << "El promedio de las alturas es: " << promedio <<" metros."<<endl; 
 
-    for (int i=0; i<n; i++){ 
+    for (int i=0; i<n; i++){ //Ciclo que realizara el conteo de que estaturas estan por debajo o encima del promedio
         if (promedio<altura[i]){
             arriba++;
     } else if (promedio>altura[i]) {
             abajo++;
     }
     } 
-    cout << "Hay " << arriba <<" alturas por encima del promedio. "<<endl;
+    cout << "Hay " << arriba <<" alturas por encima del promedio. "<<endl; 
     cout << "Hay " << abajo <<" alturas por debajo del promedio. "<<endl;
   
 }
@@ -44,7 +44,7 @@ int main(){
 
     cout << "Programa para calcular el promedio de altura de los estudiantes"<<endl;
     
-   estaturaPromedio(n, suma, altura);
+   estaturaPromedio(n, suma, altura);//Llamando a la funcion
 
    system("PAUSE");
    return 0;
